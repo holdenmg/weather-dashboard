e37d61416eab4fd6580e0d71086bfceb69641ea7
Holden Garrison <holdengarrison@holdens-mbp-2.lan>
2023-07-13 20:56:45 -0400

    Added disoplayForrecast button that displays 5 day forecast by creating divs and a's

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var cityInputEl = document.querySelector('#city');
 var userFormEl = document.querySelector('#user-form');
+var forecastEl = document.querySelector('#forecast');
 
 
 var formSubmitHandler = function (event) {
@@ -52,7 +53,7 @@ var getCity = function (lat, lon) {
        console.log(response);
        response.json().then(function (data) {
          console.log(data);
-         //displayRepos(data, city);
+         displayForecast(data);
        });
      } else {
        alert('Error: ' + response.statusText);
@@ -61,5 +62,49 @@ var getCity = function (lat, lon) {
    .catch(function (error) {
      alert('Unable to connect to open weather');
    });
+   
+   function displayForecast(data){
+        var i = 4;
+        while( i < data.list.length){
+            var date = data.list[i].dt_txt
+            var icon = data.list[i].weather[0].icon
+            var temp = data.list[i].main.temp;
+            var wind = data.list[i].wind.speed;
+            var humid = data.list[i].main.humidity;
+
+            console.log(date);
+            console.log(icon);
+            console.log(temp);
+            console.log(wind);
+            console.log(humid);
+
+            var dayDiv = document.createElement('div');
+            var aDate = document.createElement('a')
+            var aIcon = document.createElement('img');
+            var aTemp = document.createElement('a');
+            var aWind = document.createElement('a');
+            var aHumid = document.createElement('a');
+            aDate.textContent = date;
+            aIcon.innerHTML = "<src = http://openweathermap.org/img/w/"+ icon +".png>";
+            aTemp.textContent = "Temp: " + temp;
+            aWind.textContent = "Wind: " + wind +" MPH";
+            aHumid.textContent = "Humidity: " + humid + "%";
+
+            dayDiv.appendChild(aDate);
+            dayDiv.appendChild(aIcon);
+            dayDiv.appendChild(aTemp);
+            dayDiv.appendChild(aWind);
+            dayDiv.appendChild(aHumid);
+
+
+            forecastEl.appendChild(dayDiv)
+
+            //add classes here//
+
+
+            i = i+8
+        }
+
+   }
 }
   userFormEl.addEventListener('submit', formSubmitHandler);
